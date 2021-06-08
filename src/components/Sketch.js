9247a28e0836b2a2cec0abbe517aa87f26f07e56
Tyler Farris <Tylerpfarris@gmail.com>
2021-06-08 12:50:09 -0600

    added heroku domain

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -10,7 +10,7 @@ const socket = io('https://salty-chamber-55261.herokuapp.com/',
         transportOptions: {
             polling: {
                 extraHeaders: {
-                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Origin': 'https://salty-chamber-55261.herokuapp.com/',
                     'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
                     'Access-Control-Allow-Headers': 'req-header',
                     'Access-Control-Allow-Credentials': true
