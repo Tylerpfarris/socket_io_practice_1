f1026129c0fc5170037c958e284c5e6ac46d00ec
Tyler Farris <Tylerpfarris@gmail.com>
2021-06-07 22:13:56 -0600

    init

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -1,34 +1,74 @@
-import React, { createRef, useEffect } from 'react';
-import p5 from 'p5';
+import React, { useEffect, useRef } from 'react';
+import P5 from 'p5';
+import io from 'socket.io-client';
+const socket = io('https://salty-chamber-55261.herokuapp.com/', {
+    withCredentials: true,
+});
+
+
+// // Start the socket connection
+
+
+// // Callback function
+
+
 
 export const SketchComponent = () => {
-    const myRef = createRef();
+    // const colorRef = useRef(null);
+    // const lineWeightRef = useRef(null);
+    const color = '#FF0000'; 
+    const strokeWidth = 4;
+    // const myRef = useRef();
+    const canvasRef = useRef();
 
+    useEffect(() => {
+        // myRef.current = io();
+        const myp5 = new P5(Sketch, canvasRef.current);
+
+       
+        return myp5;
+    
+    }, []);
+
+   
     const Sketch = (p) => {
         p.setup = () => {
             p.createCanvas(800, 800);
-        };
-        let xS = 0;
-        let yS = 0;
+            p.background(255, 255, 255);
+            
+            socket.on('mouse response', data => {
+                p.stroke(data.color);
+                p.strokeWeight(data.strokeWidth);
+                p.line(data.x, data.y, data.px, data.py);
+            });
 
-        p.draw = () => {
-            p.background(0);
-            p.colorMode(p.HSB);
-            p.fill(yS / 4, 255, 255);
-            p.noStroke();
-            p.noCursor();
-            p.ellipse(p.mouseX, p.mouseY, xS);
-            if(xS > 800) xS = 0;
-            if(yS > 800) yS = 0;
-            xS++;
-            yS++;
         };
+
+        p.mouseDragged = () => {
+            p.stroke('#FF0000');
+            p.strokeWeight(4);
+            p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+            sendMouse(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+        }; 
+        function sendMouse(x, y, pX, pY) {
+            const data = {
+                x,
+                y,
+                px: pX,
+                py: pY,
+                color,
+                strokeWidth,
+            };
+
+            socket.emit('transmit mouse', data);
+            console.log(socket.emit('transmit mouse', data));
+      
+        }
+
+
     };
+    return (
+        <div ref={canvasRef}></div>
 
-    useEffect(() => {
-        const myp5 = new p5(Sketch, myRef.current);
-        return myp5;
-    }, []);
-
-    return <div ref={myRef}></div>;
+    );
 };
