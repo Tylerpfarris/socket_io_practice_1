418d3a9194684b8e08a511a09caa79a5bae84f78
Tyler Farris <Tylerpfarris@gmail.com>
2021-06-08 10:50:22 -0600

    added headers for cors

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -1,11 +1,19 @@
+// https://cors-anywhere.herokuapp.com/
+
 import React, { useEffect, useRef } from 'react';
 import P5 from 'p5';
 import io from 'socket.io-client';
-const socket = io('https://cors-anywhere.herokuapp.com/https://salty-chamber-55261.herokuapp.com/', {
-    cors: {
-        origin: ['https://modest-hoover-a49330.netlify.app', 'https://salty-chamber-55261.herokuapp.com/', 'http://modest-hoover-a49330.netlify.app', 'http://salty-chamber-55261.herokuapp.com/']
+const socket = io('https://salty-chamber-55261.herokuapp.com/', {
+    withCredentials: true,
+    transportOptions: {
+        polling: {
+            extraHeaders: {
+                'my-custom-header': 'drawingApp'
+            }
+        }
     }
-});
+}
+);
 
 
 // // Start the socket connection
